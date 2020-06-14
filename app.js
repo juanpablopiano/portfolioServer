9b71151583ca7c568c2a0add0eeae0be24ee0692
Juan Gonzalez <jgu1@hotmail.com>
2020-06-13 20:13:37 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+
+app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.json({
+		hola: "Hola mundo",
+		name: "Juan",
+		lastName: "Gonzalez",
+		title: "Portfolio",
+	});
+});
+
+app.listen(process.env.PORT || 3000, () =>
+	console.log("Portfolio is listening.")
+);
